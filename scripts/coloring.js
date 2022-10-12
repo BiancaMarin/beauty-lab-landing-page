f67576a82a0350bfb2873460ee6ddf7cdb448883
Bianca Marin <biancamarin12@yahoo.com>
2022-10-12 14:17:40 +0300

    beauty lab

---
+++ scripts/coloring.js
@@ -0,0 +1,35 @@
+const gallery = document.querySelector('[data-gallery]');
+const displayImage = document.querySelector('[data-display]');
+const imageArray = [1, 2, 3, 4, 5, 6];
+
+imageArray.forEach((i) => {
+  const img = document.createElement('img');
+  img.src = `/images/color-${i}.jpg`;
+  img.alt = `Image nr. ${i}`;
+  img.classList.add('gallery');
+  gallery.appendChild(img);
+});
+
+const lightbox = document.createElement('div');
+lightbox.id = 'lightbox';
+displayImage.appendChild(lightbox);
+
+const images = document.querySelectorAll('img');
+images.forEach((image) => {
+  image.addEventListener('click', (e) => {
+    lightbox.classList.add('active');
+    const newImg = document.createElement('img');
+    newImg.classList.add('newImg');
+    newImg.src = image.src;
+
+    if (lightbox.firstChild) {
+      lightbox.removeChild(lightbox.firstChild);
+    }
+    lightbox.appendChild(newImg);
+  });
+});
+
+lightbox.addEventListener('click', (e) => {
+  if (e.target !== e.currentTarget) return;
+  lightbox.classList.remove('active');
+});
